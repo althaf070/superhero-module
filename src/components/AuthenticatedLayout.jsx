61763bdb3f7dee91cd4065b434fcf9a1d292cc67
Muhammed Althaf <Mdalthaf424@gmail.com>
2024-11-20 09:11:55 +0530

    add-loader

--- src/components/AuthenticatedLayout.jsx
+++ src/components/AuthenticatedLayout.jsx
@@ -4,12 +4,14 @@ import Navbar from './Navbar'
 import { SidebarTrigger } from './ui/sidebar'
 import { useAuthStore } from '@/store/useAuthStore'
 import { useEffect } from 'react'
+import { Loader } from 'lucide-react'
 
 const AuthenticatedLayout = () => {
-  const {isAuthenticated,checkAuth} = useAuthStore()
+  const {isAuthenticated,checkAuth,isCheckingAuth} = useAuthStore()
 useEffect(() => {
 checkAuth()
 }, [checkAuth])
+if(isCheckingAuth) return <Loader className='animate-spin ' size={44}/>
 if(!isAuthenticated) return <Navigate to={'/login'} replace/>
   return (
     <>
